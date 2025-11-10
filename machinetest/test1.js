6b908d2d22634b5fc9770044ad1dbc60c1ee09b5
jerinmt <jerinmt379@gmail.com>
2025-11-10 19:53:51 +0530

    Completed machinetest 1

---
+++ machinetest/test1.js
@@ -0,0 +1,40 @@
+function calculate(event) {
+    event.preventDefault();//to avoid refreshing the page
+    const firstNum = Number(document.getElementById('first').value);
+    const secondNum = Number(document.getElementById('second').value);
+    const operator = document.getElementById('operator').value;
+    switch(operator) {
+        case '+':
+            add(firstNum, secondNum);    
+        break;
+        case '-':
+            subtract(firstNum, secondNum);
+        break;
+        case '*':
+            multiply(firstNum, secondNum);
+        break;
+        case '/':
+            divide(firstNum, secondNum);
+        break;
+    }
+}
+function add(first, second) {
+    let result = first + second;
+    printResult(result);
+}
+function subtract(first, second) {
+    let result = first - second;
+    printResult(result);
+}
+function multiply(first, second) {
+    let result = first * second;
+    printResult(result);
+}
+function divide(first, second) {
+    let result = first / second;
+    printResult(result);
+}
+function printResult(result) {
+    const output = document.getElementById('result');
+    output.innerHTML = result;
+}
